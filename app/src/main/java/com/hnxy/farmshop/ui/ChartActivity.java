3d9e64421ad1ea3c6fca262037c41b488f35b58b
chenchen211 <chendaolong1993@163.com>
2017-09-04 15:02:59 +0800

    图表

---
+++ app/src/main/java/com/hnxy/farmshop/ui/ChartActivity.java
@@ -0,0 +1,15 @@
+package com.hnxy.farmshop.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hnxy.farmshop.R;
+
+public class ChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+    }
+}
